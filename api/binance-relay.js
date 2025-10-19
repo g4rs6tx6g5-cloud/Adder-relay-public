e7fd7a5fa620ad6c97229060eb34cca300ed9c04
g4rs6tx6g5-cloud <g4rs6tx6g5@privaterelay.appleid.com>
2025-10-19 16:33:41 +0100

    Update binance-relay.js

--- api/binance-relay.js
+++ api/binance-relay.js
@@ -1,4 +1,5 @@
-module.exports = async (req, res) => {
+import fetch from 'node-fetch';
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
